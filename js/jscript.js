b7aa78ac57f3f57cbc73027f49a3e12ea9348e5d
Chuck Kang <chuckkang@gmail.com>
2017-08-21 17:12:50 -0700

    Fancy Array

---
+++ js/jscript.js
@@ -0,0 +1,21 @@
+
+function fancyarray(arr, reversed)
+{
+
+    if (reversed === true)
+        {
+        for (var i = arr.length-1; i >= 0 ; i--) {
+            console.log(i + " --> " + arr[i])
+        }
+        }
+    else
+        {
+    for (var i = 0; i< arr.length; i++){
+        console.log(i + " --> " + arr[i])
+    }
+    }
+    
+}
+
+var arrNames =  ['chuck', 'bob', 'chris', 'ted'];
+fancyarray(arrNames, true);
